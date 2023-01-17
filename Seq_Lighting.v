47a26bef6cbb41fe18ac566cf813212ff0db6574
AhwuIOT <121588389+AhwuIOT@users.noreply.github.com>
2023-01-17 14:06:06 +0800

    Update Seq_Lighting.v

--- Seq_Lighting.v
+++ Seq_Lighting.v
@@ -1,7 +1,7 @@
 module Seq_Lighting(clk, reset, a, b, c, d, e);
 	input	clk, reset;
-	reg		[31:0]divider;
-	reg 	[2:0]counter;
+	reg	[31:0]	divider;
+	reg 	[2:0]	counter;
 	output	reg	a, b, c, d, e;
 
 	
@@ -30,14 +30,14 @@ module Seq_Lighting(clk, reset, a, b, c, d, e);
 	end
 	
 	always@(*)
-		begin
-			case(counter)
-				3'b001: {a, b, c, d, e} <= 5'b10000;
-				3'b010: {a, b, c, d, e} <= 5'b01000;
-				3'b011: {a, b, c, d, e} <= 5'b00100;
-				3'b100: {a, b, c, d, e} <= 5'b00010;
-				3'b101: {a, b, c, d, e} <= 5'b00001;
-				default:{a, b, c, d, e} <= 5'b00000;
-			endcase
-		end
+	begin
+		case(counter)
+			3'b001: {a, b, c, d, e} <= 5'b10000;
+			3'b010: {a, b, c, d, e} <= 5'b01000;
+			3'b011: {a, b, c, d, e} <= 5'b00100;
+			3'b100: {a, b, c, d, e} <= 5'b00010;
+			3'b101: {a, b, c, d, e} <= 5'b00001;
+			default:{a, b, c, d, e} <= 5'b00000;
+		endcase
+	end
 endmodule
